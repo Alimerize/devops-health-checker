7232f9105e95259407601f3634799b6fb4ab8663
Alisa Merz <132124716+Alimerize@users.noreply.github.com>
2026-05-12 12:23:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,15 @@
 
 echo "Начинаю настройку прав доступа для Health Checker..."
 
-# 1. Создание лог-файла (если его нет)
 LOG_PATH="/var/log/health_checker.log"
 if [ ! -f "$LOG_PATH" ]; then
     sudo touch $LOG_PATH
     echo "Файл лога создан."
 fi
 
-# 2. Настройка владельца (Тема: chown)
-# Мы отдаем файл текущему пользователю, чтобы скрипт мог в него писать
 sudo chown $USER:$USER $LOG_PATH
 echo "Права владельца на лог-файл переданы пользователю $USER."
 
-# 3. Настройка прав на запуск скрипта (Тема: chmod)
 chmod +x checker.py
 echo "Скрипт checker.py теперь исполняемый (755)."
 
